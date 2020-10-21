74d9748f33e6bc82840b289654723e16bdd66090
johannamag <69107842+johannamag@users.noreply.github.com>
2020-10-21 09:23:13 +0200

    Installed Firebase

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBLSHsvng1dDgFlHSpX2bqETbT4yJU7r4k",
+  authDomain: "react-yt-clone-johannamag.firebaseapp.com",
+  databaseURL: "https://react-yt-clone-johannamag.firebaseio.com",
+  projectId: "react-yt-clone-johannamag",
+  storageBucket: "react-yt-clone-johannamag.appspot.com",
+  messagingSenderId: "364606177418",
+  appId: "1:364606177418:web:52fbb75a65116e7fd1ad73",
+};
