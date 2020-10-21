30db5e549564f3f52b254dcf3e7f2130a5c5fdf3
johannamag <69107842+johannamag@users.noreply.github.com>
2020-10-21 13:38:24 +0200

    Recommended Videos

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,143 @@
+import React from "react";
+import "./RecommendedVideos.css";
+import Video from "./Video";
+
+function RecommendedVideos() {
+  return (
+    <div className="recommendedVideos">
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <Video
+          title="How to Body Build in 4 easy Steps"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://www.fotojet.com/template-imgs/social-media-header/youtube-thumbnail/build-body-youtube-thumbnail.jpg"
+        />
+        <Video
+          title="Lorem Ipsum"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://ytexpert.net/wp-content/uploads/2019/11/How-To-Make-An-Eye-Catching-Thumbnail-For-More-Clicks-862x485.jpg"
+        />
+        <Video
+          title="Lorem Ipsum"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://www.tubics.com/wp-content/uploads/2018/10/how-to-create-youtube-thumbnails-1.png"
+        />
+        <Video
+          title="Lorem Ipsum"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://i.ytimg.com/vi/aZVo8aJmjfc/maxresdefault.jpg"
+        />
+        <Video
+          title="Lorem Ipsum"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/110240557/original/2c78694fb3b65f430624f4c0035bec2320dea2ba/create-two-catchy-fortnite-and-gaming-youtube-thumbnail-or-banner-art.jpg"
+        />
+        <Video
+          title="Lorem Ipsum"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs2/160748763/original/ff7c041c744097bc9bc0711d4307e4e259eabaaf/design-outstanding-lyric-prank-thumbnails-for-cheap.png"
+        />
+        <Video
+          title="Lorem Ipsum"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://www.fotojet.com/template-imgs-thumb/design/youtube-thumbnail/family-picnic-youtube-thumbnail.jpg"
+        />
+        <Video
+          title="Lorem Ipsum"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://img0-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/29807/optimized_large_thumb_stage.jpg"
+        />
+        <Video
+          title="How to Body Build in 4 easy Steps"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://www.fotojet.com/template-imgs/social-media-header/youtube-thumbnail/build-body-youtube-thumbnail.jpg"
+        />
+        <Video
+          title="Lorem Ipsum"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://ytexpert.net/wp-content/uploads/2019/11/How-To-Make-An-Eye-Catching-Thumbnail-For-More-Clicks-862x485.jpg"
+        />
+        <Video
+          title="Lorem Ipsum"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://www.tubics.com/wp-content/uploads/2018/10/how-to-create-youtube-thumbnails-1.png"
+        />
+        <Video
+          title="Lorem Ipsum"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://i.ytimg.com/vi/aZVo8aJmjfc/maxresdefault.jpg"
+        />
+        <Video
+          title="Lorem Ipsum"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/110240557/original/2c78694fb3b65f430624f4c0035bec2320dea2ba/create-two-catchy-fortnite-and-gaming-youtube-thumbnail-or-banner-art.jpg"
+        />
+        <Video
+          title="Lorem Ipsum"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs2/160748763/original/ff7c041c744097bc9bc0711d4307e4e259eabaaf/design-outstanding-lyric-prank-thumbnails-for-cheap.png"
+        />
+        <Video
+          title="Lorem Ipsum"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://www.fotojet.com/template-imgs-thumb/design/youtube-thumbnail/family-picnic-youtube-thumbnail.jpg"
+        />
+        <Video
+          title="Lorem Ipsum"
+          views="23K Views"
+          timestamp="11 hours ago"
+          channelImage="https://bridgestolifedetox.com/wp-content/uploads/2020/03/couple.jpg"
+          channel="FAM"
+          image="https://img0-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/29807/optimized_large_thumb_stage.jpg"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default RecommendedVideos;
